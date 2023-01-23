a0a8384b06c9223811ea50225d77d2781a50674d
Charahiro-tan <73784718+Charahiro-tan@users.noreply.github.com>
2023-01-24 03:43:21 +0900

    rewrite

--- src/main.js
+++ src/main.js
@@ -1,45 +1,64 @@
-const localStorageKey = 'TabRemember'
-const forYouSelector = '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1e5uvyk.r-5zmot > div:nth-child(2) > nav > div > div.css-1dbjc4n.r-1adg3ll.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1udh08x > div > div:nth-child(1) > a';
-const followingSelector = '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1e5uvyk.r-5zmot > div:nth-child(2) > nav > div > div.css-1dbjc4n.r-1adg3ll.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1udh08x > div > div:nth-child(2) > a';
+const localStoragePrefix = 'TabRemember'
+const homeTabSelector = '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1e5uvyk.r-5zmot > div:nth-child(2) > nav > div > div.css-1dbjc4n.r-1adg3ll.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1udh08x > div > div > a';
 
-const forYouClick = () => {
-    localStorage.setItem(localStorageKey, 'foryou');
-}
+const sleep = ms => new Promise(s => setTimeout(s, ms));
 
-const followingClick = () => {
-    localStorage.setItem(localStorageKey, 'following');
-}
+let lasthref;
 
-const main = async () => {
-    let forYouElm;
-    let followElm;
-    for (i = 1; i < 11; i++) {
-        await new Promise(s => setTimeout(s, i * 100));
-        if (!forYouElm) {
-            forYouElm = document.querySelector(forYouSelector);
+const getTabList = async selector => {
+    let tabList = [];
+    while (true) {
+        await sleep(100);
+        const doms = document.querySelectorAll(selector);
+        if (!doms.length) {
+            continue;
         }
-        if (!followElm) {
-            followElm = document.querySelector(followingSelector);
+        if (tabList.length !== doms.length) {
+            tabList = doms;
+            await sleep(100);
+            continue;
         }
-        if (forYouElm && followElm) {
+        if (tabList.length === doms.length) {
             break;
         }
     }
-    if (!forYouElm || !followElm) {
-        return;
+    return tabList;
+}
+
+const homeTabClick = (e) => {
+    let elm = e.srcElement;
+    if (elm.localName !== 'a') {
+        elm = elm.closest('a');
     }
+    localStorage.setItem(localStoragePrefix + '_homeIndex', elm.tabRememberIndex);
+}
 
-    forYouElm.addEventListener('click', forYouClick);
-    followElm.addEventListener('click', followingClick);
-
-    const ls = localStorage.getItem(localStorageKey);
+const home = async () => {
+    const tabList = await getTabList(homeTabSelector);
+    for (i = 0; i < tabList.length; i++) {
+        tabList[i].tabRememberIndex = i;
+        tabList[i].addEventListener('click', homeTabClick);
+    }
+    const ls = localStorage.getItem(localStoragePrefix + '_homeIndex');
     if (ls) {
-        if (ls === 'following') {
-            followElm.click();
-        } else if (ls === 'foryou') {
-            forYouElm.click();
+        const index = Number(ls);
+        if (index < tabList.length) {
+            tabList[index].click();
         }
     }
 }
 
-window.addEventListener("load", main)
+const main = () => {
+    if (lasthref === location.href) {
+        return;
+    }
+    lasthref = location.href;
+    switch (location.pathname) {
+        case '/home':
+            home();
+            break;
+    }
+}
+
+const observer = new MutationObserver(main);
+observer.observe(document, { childList: true, subtree: true });
